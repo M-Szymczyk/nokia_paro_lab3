742a72675e0da87605e301f78a341f9fe0e4f564
Mikolaj Szymczyk <mikolaj.szymczyk.99@gmail.com>
2022-03-29 21:56:12 +0200

    fix example3

--- example3.cpp
+++ example3.cpp
@@ -1,45 +1,49 @@
+
+
 #include <iostream>
 #include <stdexcept>
 
-void validateArguments(int argc)
-{
-    if(argc != 2)
-    {
+class myPersonalException : public std::logic_error {
+public:
+    explicit myPersonalException(const std::string &arg) : logic_error(arg) {}
+};
+
+void validateArguments(int argc) {
+    if (argc != 2) {
         std::cerr << "You need to pass 1 argument" << std::endl;
         exit(-1);
     }
 }
 
-class Resource
-{
+class Resource {
 public:
-    void use(const char* arg)
-    {
+    void use(const char *arg) {
         std::cout << "Using resource. Passed " << *arg << std::endl;
-        if (*arg == 'd')
-        {
-            throw std::logic_error("Passed d. d is prohibited.");
+        if (*arg == 'd') {
+            throw myPersonalException("Passed d. d is prohibited.");
         }
     }
 };
 
-int main(int argc, char* argv[])
-{
+int main(int argc, char *argv[]) {
     validateArguments(argc);
 
-    const char* argument = argv[1];
-    Resource* rsc = nullptr;
+    const char *argument = argv[1];
+    Resource *rsc = nullptr;
 
-    try
-    {
+
+
+    try {
         rsc = new Resource();
         rsc->use(argument);
-        delete rsc;
     }
-    catch (std::logic_error& e)
-    {
+    catch (myPersonalException &e) {
         std::cout << e.what() << std::endl;
     }
+    if (rsc != nullptr) {
+        delete rsc;
+    }
+
     return 0;
 }
 
